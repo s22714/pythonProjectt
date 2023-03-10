5770b58fff749881ddc82724b78e2184faa4ae33
s22714 <s22714@pjwstk.edu.pl>
2023-03-10 19:22:30 +0100

    finished homework

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import webbrowser
 import requests
 
-pageUlr = 'http://github.com'
-webbrowser.open(pageUlr)
+#pageurl = 'http://github.com'
+#date = 20150101
+
+pageurl = input("podaj adres: ")
+date = input("podaj date format YYYYMMDD: ")
+
+for a in range (3):
+    url ="http://archive.org/wayback/available?url="+pageurl+"&timestamp="+str(date)
+    response = requests.get(url)
+    d = response.json()
+    page = d["archived_snapshots"]["closest"]["url"]
+    webbrowser.open(page)
 
